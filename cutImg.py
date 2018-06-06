18b5584c265a97359a3fbaac186ee1da1ca902ea
AlexQianYi <q563875735@gmail.com>
2018-06-06 15:57:01 +0800

    cut image data set

--- cutImg.py
+++ cutImg.py
@@ -1,15 +1,16 @@
 from PIL import Image
 import os
 
+file_origin = './BinPic'
 
 def get_children_img(img):
 
     ChildImgList = []
 
     for i in range(5):
-        x = 9 + i * (16+14)
+        x = 9 + i * (22+8)
         y = 9
-        ChildImg = img.crop((x, y, x+16, y+34))
+        ChildImg = img.crop((x, y, x+22, y+34))
         ChildImgList.append(ChildImg)
 
     return ChildImgList
@@ -28,6 +29,22 @@ def save_children_img(path, ChildImgList):
         i+=1
 
 if __name__ == "__main__" :
+
+    count = 0
+    names = os.listdir(file_origin)
+
+
+    for file in names:
+        fileNum = file[:-9]
+        info = "cut " + str(count+1) + "pic : " + fileNum
+        print(info)
+
+        img = Image.open('./BinPic/' + file).convert('L')
+
+        ChildImgList = get_children_img(img)
+        save_children_img(fileNum, ChildImgList)
+
+        count += 1
 
     img = Image.open('82image.jpg')
 
